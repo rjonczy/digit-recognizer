0d1e5e90e7ef608cd943b7c629f7fff3027a6ef4
Robert Jonczy <robert.jonczy@gmail.com>
2016-02-07 18:35:23 +0100

    changed data input/output folders

--- rf.R
+++ rf.R
@@ -7,8 +7,8 @@ set.seed(1234)
 numTrain <- 10000
 numTrees <- 25
 
-train <- read_csv("./data/train.csv")
-test <- read_csv("./data/test.csv")
+train <- read_csv("./data/input/train.csv")
+test <- read_csv("./data/input/test.csv")
 
 rows <- sample(1:nrow(train), numTrain)
 labels <- as.factor(train[rows, 1])
@@ -18,5 +18,5 @@ rf <- randomForest(train, labels, xtest = test, ntree = numTrees)
 predictions <- data.frame(ImageId = 1:nrow(test), Label = levels(labels)[rf$test$predicted])
 head(predictions[,2])
 
-write_csv(predictions, "rf_benchmark.csv") 
+write_csv(predictions, "./data/output/rf.csv") 
 
